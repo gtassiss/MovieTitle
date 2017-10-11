50304d0806adde64fd0a7336b88ddb845b286e0e
gtassiss <gtassiss@montgomerycollege.edu>
2017-10-11 13:43:26 -0400

    Initial commit

---
+++ src/MoviTitle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class MoviTitle {
+    public static void main(String[] args) {
+
+
+            String Movie = "";
+            String result = "";
+            int count= 1;
+            String.repeat();
+
+
+        while (result != "quit") ;
+        {
+            System.out.print(" Enter a movie title:");
+            Scanner keyboard = new Scanner(System.in);
+            Movie = keyboard.next();
+            System.out.println("The Movie is:" + Movie);
+
+
+
+
+
+
+
+
+    }}}
+
+
+
